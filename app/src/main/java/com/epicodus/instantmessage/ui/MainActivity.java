eebdd8493901b440f3d75e78b85d67f84f4b549e
Jake Steinberg <jakesteinberg@gmail.com>
2016-05-04 21:51:27 -0700

    add ability to log out

--- app/src/main/java/com/epicodus/instantmessage/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/instantmessage/ui/MainActivity.java
@@ -1,15 +1,59 @@
 package com.epicodus.instantmessage.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.epicodus.instantmessage.Constants;
 import com.epicodus.instantmessage.R;
+import com.firebase.client.Firebase;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private Firebase mFirebaseRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    protected void logout() {
+        mFirebaseRef.unauth();
+        takeUserToLoginScreenOnUnAuth();
+    }
+
+    private void takeUserToLoginScreenOnUnAuth() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
+
+
